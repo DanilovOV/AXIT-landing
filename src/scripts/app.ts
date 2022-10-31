94e1dde6c03b24eaae5eddd62f9200256abbc5d8
Oleg <oleg-danilov8@ya.ru>
2022-10-31 23:29:23 +0300

    add: typescript

---
+++ src/scripts/app.ts
@@ -0,0 +1,7 @@
+import mobileMenu from './mobileMenu';
+import smoothScroll from './smoothScroll';
+import imageTabs from './imageTabs';
+
+mobileMenu();
+smoothScroll();
+imageTabs();
---
+++ src/scripts/mobileMenu.ts
@@ -0,0 +1,14 @@
+export default function mobileMenu() {
+    const burger = document.querySelector('.js-header-burger');
+    const headerMenu = document.querySelector('.js-header-menu');
+    const body = document.querySelector('body');
+
+    burger?.addEventListener('click', toggleMobileMenu);
+
+    function toggleMobileMenu() {
+        burger?.classList.toggle('active');
+        headerMenu?.classList.toggle('active');
+        body?.classList.toggle('body--fixed');
+    }
+}
+
