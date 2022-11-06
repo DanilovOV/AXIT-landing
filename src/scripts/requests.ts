50378ddbe45fd054eda96378849205a413b2f870
Oleg <oleg-danilov8@ya.ru>
2022-11-07 01:26:13 +0300

    add: axios

--- src/scripts/requests.ts
+++ src/scripts/requests.ts
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+
+
 export default function requests() {
 
     const url = 'https://628e4808a339dfef87ab4f4b.mockapi.io/'
@@ -7,51 +11,37 @@ export default function requests() {
     function trialFormSubmit(e: Event) {
         e.preventDefault()
 
-        let userData = {
+        axios.post('/user', {
             name:     (<HTMLInputElement>document.getElementById('trial-text')).value,
             email:    (<HTMLInputElement>document.getElementById('trial-email')).value,
             password: (<HTMLInputElement>document.getElementById('trial-password')).value,
-        };
-
-        fetch(url + 'AXIT-trial-requests', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify(userData)
         })
-        .then(response => {
-            if (!response.ok) throw new Error()
+        .then(function (response) {
+            alert(response)
         })
-        .then(() => alert('Success!'))
-        .catch(() => alert('Error'))
+        .catch(function (error) {
+            alert(error);
+        });
     }
 
 
 
-    // Отправка данных формы в блоке Contact
+    // Отправка формы в блоке Contact
     document.querySelector('.js-send-message').addEventListener('click', contactFormSubmit)
     function contactFormSubmit(e:Event) {
         e.preventDefault()
 
-        let userData = {
+        axios.post('/user', {
             name:    (<HTMLInputElement>document.getElementById('contact-name')).value,
             email:   (<HTMLInputElement>document.getElementById('contact-email')).value,
             subject: (<HTMLInputElement>document.getElementById('contact-subject')).value,
             message: (<HTMLInputElement>document.getElementById('contact-message')).value
-        }
-
-        fetch(url + 'AXIT-messages', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            },
-            body: JSON.stringify(userData)
         })
-        .then(response => {
-            if (!response.ok) throw new Error()
+        .then(function (response) {
+            alert(response)
         })
-        .then(() => alert('Success!'))
-        .catch(() => alert('Error'))
+        .catch(function (error) {
+            alert(error);
+        });
     }
 }
